aa183865ad1079e29333c00d8743e81607c0e1de
luandapeng <luandapeng@highwho.com>
2018-04-25 11:24:46 +0800

    添加按需加载model component

--- src/router.js
+++ src/router.js
@@ -1,14 +1,54 @@
-import React from 'react';
-import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage';
+import React, { createElement } from 'react';
+import { Route, Switch, Redirect, routerRedux } from 'dva/router';
+import dynamic from 'dva/dynamic';
+import Error from './components/Error';
 
-function RouterConfig({ history }) {
+dynamic.setDefaultLoadingComponent(() => {
+  return <div>loading</div>;
+});
+
+const { ConnectedRouter } = routerRedux;
+
+const modelNotExisted = (app, model) => (
+  // eslint-disable-next-line
+  !app._models.some(({ namespace }) => {
+    return namespace === model.substring(model.lastIndexOf('/') + 1);
+  })
+);
+
+const dynamicWrapper = (app, models, component) => {
+  // () => import()
+  return dynamic({
+    app,
+    models: () => models.filter(
+      model => modelNotExisted(app, model)).map(m => import(`../models/${m}.js`)
+    ),
+    component: () => {
+      return component().then((raw) => {
+        const Component = raw.default || raw;
+        return props => createElement(Component, props);
+      });
+    },
+  });
+};
+
+function RouterConfig({ history, app }) {
+  // route config
+  const route = {
+    '/example': {
+      component: dynamicWrapper(app, ['user', 'login'], () => import('../routes/IndexPage')),
+    },
+  };
   return (
-    <Router history={history}>
+    <ConnectedRouter history={history}>
       <Switch>
-        <Route path="/" exact component={IndexPage} />
+        <Redirect exact from="/" to="example" />
+        {Object.keys(route).map(path => (
+          <Route path={path} exact component={route[path].component} />
+        ))}
+        <Route component={Error} />
       </Switch>
-    </Router>
+    </ConnectedRouter>
   );
 }
 
